78d49272f06aad9b567b060a2ef151ea60bfb9ad
Sonny-2k <Sonny-2k@users.noreply.github.com>
2020-10-15 17:04:00 +0700

    before add feature signup/login

--- App.js
+++ App.js
@@ -1,21 +1,328 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Animated, Easing, Image, StyleSheet  } from 'react-native';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { AppStyles, Configuration } from './AppStyles'
+
+import LoginScreen from './screens/LoginScreen'
+import SignupScreen from './screens/SignupScreen'
+import WelcomeScreen from './screens/WelcomeScreen'
+import HomeScreen from './screens/HomeScreen'
+
+import DrawerContainer from './components/DrawerContainer'
+
+
+
+// const LoginStack = createStackNavigator(
+//   {
+//     Login: { screen: LoginScreen },
+//     Signup: { screen: SignupScreen },
+//     Welcome: { screen: WelcomeScreen }
+//   },
+//   {
+//     initialRouteName: "Welcome",
+//     headerMode: "float",
+//     navigationOptions: ({ navigation }) => ({
+//       headerTintColor: "red",
+//       headerTitleStyle: styles.headerTitleStyle
+//     }),
+//     cardStyle: { backgroundColor: "#FFFFFF" }
+//   }
+// );
+
+const LoginStack = createStackNavigator()
+
+function Login() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginStack.Navigator initialRouteName="Welcome"  headerMode="float" >
+      <LoginStack.Screen name="Login" component={LoginScreen} options={({navigation}) => ({
+        title: '',
+        headerTitleStyle: styles.headerTitleStyle,
+        cardStyle: { backgroundColor: "#FFFFFF"}
+      })}/>
+      <LoginStack.Screen name="Signup" component={SignupScreen} options={({navigation}) => ({
+        title: '',
+        headerTitleStyle: styles.headerTitleStyle,
+        cardStyle: { backgroundColor: "#FFFFFF"}
+      })}/>
+      <LoginStack.Screen name="Welcome" component={WelcomeScreen} options={({navigation}) => ({
+        title: '',
+        headerTitleStyle: styles.headerTitleStyle,
+        cardStyle: { backgroundColor: "#FFFFFF"}
+      })}/>
+    </LoginStack.Navigator>
+  )
+}
+
+const HomeStack = createStackNavigator()
+
+function Home() {
+  return (
+    <HomeStack.Navigator initialRouteName="Home" headerMode="float" headerLayoutPreset="center">
+      <HomeStack.Screen name="Home" component={HomeScreen} options={({navigation}) => ({
+        headerTitleStyle: styles.headerTitleStyle,
+        cardStyle: { backgroundColor: "#FFFFFF"}
+      })} />
+    </HomeStack.Navigator>
+  )
+}
+
+
+
+// const HomeStack = createStackNavigator(
+//   {
+//     Home: { screen: HomeScreen }
+//   },
+//   {
+//     initialRouteName: "Home",
+//     headerMode: "float",
+
+//     headerLayoutPreset: "center",
+//     navigationOptions: ({ navigation }) => ({
+//       headerTintColor: "red",
+//       headerTitleStyle: styles.headerTitleStyle
+//     }),
+//     cardStyle: { backgroundColor: "#FFFFFF" }
+//   }
+// );
+
+const TabNavigator = createBottomTabNavigator()
+
+function MyTabs() {
+  return (
+    <TabNavigator.Navigator 
+      initialLayout={{height: 300}}
+      tabBarOptions={{
+        activeTintColor: AppStyles.color.tint,
+        inactiveTintColor: "gray",
+        style: {
+          height: Configuration.home.tab_bar_height
+        }
+      }}>
+      <TabNavigator.Screen name="Home" component={Home} options={({navigation}) => ({
+        tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = AppIcon.images.home;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return (
+          <Image
+            style={{
+              tintColor: focused ? AppStyles.color.tint : AppStyles.color.grey
+            }}
+            source={iconName}
+          />
+        );
+      }
+      })}/>
+    </TabNavigator.Navigator>
+  )
+}
+
+// const TabNavigator = createBottomTabNavigator(
+//   {
+//     Home: { screen: HomeStack }
+//   },
+  // {
+  //   navigationOptions: ({ navigation }) => ({
+  //     tabBarIcon: ({ focused, tintColor }) => {
+  //       const { routeName } = navigation.state;
+  //       let iconName;
+  //       if (routeName === "Home") {
+  //         iconName = AppIcon.images.home;
+  //       }
+
+  //       // You can return any component that you like here! We usually use an
+  //       // icon component from react-native-vector-icons
+  //       return (
+  //         <Image
+  //           style={{
+  //             tintColor: focused ? AppStyles.color.tint : AppStyles.color.grey
+  //           }}
+  //           source={iconName}
+  //         />
+  //       );
+  //     }
+  //   }),
+  //   initialLayout: {
+  //     height: 300
+  //   },
+  //   tabBarOptions: {
+  //     activeTintColor: AppStyles.color.tint,
+  //     inactiveTintColor: "gray",
+  //     style: {
+  //       height: Configuration.home.tab_bar_height
+  //     }
+  //   }
+  // }
+// );
+
+// const DrawerStack = createDrawerNavigator(
+//   {
+//     Tab: TabNavigator
+//   },
+  // {
+  //   drawerPosition: "left",
+  //   initialRouteName: "Tab",
+  //   drawerWidth: 200,
+  //   contentComponent: DrawerContainer
+  // }
+// );
+
+const Drawer = createDrawerNavigator()
+
+function MyDrawer() {
+  return (
+    // <NavigationContainer>
+      <Drawer.Navigator
+        drawerPosition="left"
+        initialRouteName= "Tab"
+        drawerWidth={200}
+        contentComponent={DrawerContainer}>
+        <Drawer.Screen name="Tab" component={MyTabs} />
+      </Drawer.Navigator>
+    // </NavigationContainer>
+  )
+}
+
+// Manifest of possible screens
+// const RootNavigator = createStackNavigator(
+//   {
+//     LoginStack: { screen: LoginStack },
+//     DrawerStack: { screen: DrawerStack }
+//   },
+//   {
+//     // Default config for all screens
+//     headerMode: "none",
+//     // initialRouteName: "DrawerStack",
+//     initialRouteName: "LoginStack",
+//     transitionConfig: noTransitionConfig,
+//     navigationOptions: ({ navigation }) => ({
+//       color: "black"
+//     })
+//   }
+// );
+
+const RootNavigator = createStackNavigator()
+
+function RootStack() {
+  return (
+    <RootNavigator.Navigator headerMode="none" initialRouteName="DrawerStack">
+      <RootNavigator.Screen name="LoginStack" component={Login} />
+      <RootNavigator.Screen name="DrawStack" component={MyDrawer} />
+    </RootNavigator.Navigator>
+  )
+}
+
+
+
+// const AppNavigation = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="home">
+//         <Stack.Screen name="home" component={HomeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+
+function App() {
+  return (
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="Home" component={LoginScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+    <NavigationContainer><RootStack /></NavigationContainer>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center"
+//   },
+//   or: {
+//     fontFamily: AppStyles.fontName.main,
+//     color: "black",
+//     marginTop: 40,
+//     marginBottom: 10
+//   },
+//   title: {
+//     fontSize: AppStyles.fontSize.title,
+//     fontWeight: "bold",
+//     color: AppStyles.color.tint,
+//     marginTop: 20,
+//     marginBottom: 20
+//   },
+//   leftTitle: {
+//     alignSelf: "stretch",
+//     textAlign: "left",
+//     marginLeft: 20
+//   },
+//   content: {
+//     paddingLeft: 50,
+//     paddingRight: 50,
+//     textAlign: "center",
+//     fontSize: AppStyles.fontSize.content,
+//     color: AppStyles.color.text
+//   },
+//   loginContainer: {
+//     width: AppStyles.buttonWidth.main,
+//     backgroundColor: AppStyles.color.tint,
+//     borderRadius: AppStyles.borderRadius.main,
+//     padding: 10,
+//     marginTop: 30
+//   },
+//   loginText: {
+//     color: AppStyles.color.white
+//   },
+//   placeholder: {
+//     fontFamily: AppStyles.fontName.text,
+//     color: "red"
+//   },
+//   InputContainer: {
+//     width: AppStyles.textInputWidth.main,
+//     marginTop: 30,
+//     borderWidth: 1,
+//     borderStyle: "solid",
+//     borderColor: AppStyles.color.grey,
+//     borderRadius: AppStyles.borderRadius.main
+//   },
+//   body: {
+//     height: 42,
+//     paddingLeft: 20,
+//     paddingRight: 20,
+//     color: AppStyles.color.text
+//   },
+//   facebookContainer: {
+//     width: AppStyles.buttonWidth.main,
+//     backgroundColor: AppStyles.color.facebook,
+//     borderRadius: AppStyles.borderRadius.main,
+//     padding: 10,
+//     marginTop: 30
+//   },
+//   facebookText: {
+//     color: AppStyles.color.white
+//   }
+// })
 const styles = StyleSheet.create({
-  container: {
+  headerTitleStyle: {
+    fontWeight: "bold",
+    textAlign: "center",
+    alignSelf: "center",
+    color: "purple",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    fontFamily: AppStyles.fontName.main
+  }
 });
+
+export default App;
---
+++ AppStyles.js
@@ -0,0 +1,143 @@
+import { Platform, StyleSheet, Dimensions } from "react-native";
+// import { Configuration } from "./Configuration";
+
+const { width, height } = Dimensions.get("window");
+const SCREEN_WIDTH = width < height ? width : height;
+const numColumns = 2;
+
+export const AppStyles = {
+  color: {
+    main: "#5ea23a",
+    text: "#696969",
+    title: "#464646",
+    subtitle: "#545454",
+    categoryTitle: "#161616",
+    tint: "#ff5a66",
+    description: "#bbbbbb",
+    filterTitle: "#8a8a8a",
+    starRating: "#2bdf85",
+    location: "#a9a9a9",
+    white: "white",
+    facebook: "#4267b2",
+    grey: "grey",
+    greenBlue: "#00aea8",
+    placeholder: "#a0a0a0",
+    background: "#f2f2f2",
+    blue: "#3293fe"
+  },
+  fontSize: {
+    title: 30,
+    content: 20,
+    normal: 16
+  },
+  buttonWidth: {
+    main: "70%"
+  },
+  textInputWidth: {
+    main: "80%"
+  },
+  fontName: {
+    main: "Arial",
+    bold: "Arial"
+  },
+  borderRadius: {
+    main: 25,
+    small: 5
+  }
+};
+
+export const AppIcon = {
+  container: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 8,
+    marginRight: 10
+  },
+  style: {
+    tintColor: AppStyles.color.tint,
+    width: 25,
+    height: 25
+  },
+  images: {
+    home: require("./assets/icons/home.png"),
+    defaultUser: require("./assets/icons/default_user.jpg"),
+    logout: require("./assets/icons/shutdown.png")
+  }
+};
+
+export const HeaderButtonStyle = StyleSheet.create({
+  multi: {
+    flexDirection: "row"
+  },
+  container: {
+    padding: 10
+  },
+  image: {
+    justifyContent: "center",
+    width: 35,
+    height: 35,
+    margin: 6
+  },
+  rightButton: {
+    color: AppStyles.color.tint,
+    marginRight: 10,
+    fontWeight: "normal",
+    fontFamily: AppStyles.fontName.main
+  }
+});
+
+export const ListStyle = StyleSheet.create({
+  title: {
+    fontSize: 16,
+    color: AppStyles.color.subtitle,
+    fontFamily: AppStyles.fontName.bold,
+    fontWeight: "bold"
+  },
+  subtitleView: {
+    minHeight: 55,
+    flexDirection: "row",
+    paddingTop: 5,
+    marginLeft: 10
+  },
+  leftSubtitle: {
+    flex: 2
+  },
+  avatarStyle: {
+    height: 80,
+    width: 80
+  }
+});
+
+export const Configuration = {
+    home: {
+      tab_bar_height: 50,
+      initial_show_count: 4,
+      listing_item: {
+        height: 130,
+        offset: 15,
+        saved: {
+          position_top: 5,
+          size: 25
+        }
+      }
+    },
+    map: {
+      origin: {
+        latitude: 37.78825,
+        longitude: -122.4324
+      },
+      delta: {
+        latitude: 0.0422,
+        longitude: 0.0221
+      }
+    },
+    // timeFormat: postTime => {
+    //   time = "";
+    //   if (postTime) {
+    //     time = moment(postTime).fromNow();
+    //   }
+    //   // time = postTime.toUTCString();
+    //   return time;
+    // }
+  };
+  
